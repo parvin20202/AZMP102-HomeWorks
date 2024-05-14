d77a2d840d2f002fc91a046d57445d9f2c72c1ec
parvin20202 <mi5shk0yb@code.edu.az>
2024-05-14 18:07:31 +0400

    add: added Juan react

---
+++ Juan React/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./layouts/Header";
+import Main from "./layouts/Main";
+import Footer from "./layouts/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
